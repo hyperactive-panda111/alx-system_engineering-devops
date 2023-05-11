9f17db92751f1ed1bdf04c1b85fcd4350e2a2217
Yaw Mensah <amomensah300@gmail.com>
2023-05-11 11:37:01 +0000

    adding files

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 #Fixes syntax error: from 'phpp' to 'php'
 
 exec { 'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path    => '/usr/local/bin.:/bin/'
+        command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+        path    => '/usr/local/bin.:/bin/'
 }
 
