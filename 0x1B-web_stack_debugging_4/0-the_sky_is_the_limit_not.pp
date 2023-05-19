ef78f645ddee0cc0c6fd79dfc93325c12068093f
Amo Mensah <amomensah300@gmail.com>
2023-05-19 11:45:20 +0000

    adding task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
-#increases traffic limit on nginx server
+# increase number of user requests
 
-exec  { 'fix-for-nginx':
-    command => 'sed -i "s/15/4096" /etc/default/nginx/',
-    path    => '/usr/local/bin/:bin/'
+exec {'replace':
+  provider => shell;
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-#restart nginx
-exec { 'nginx-restart':
-    command => 'nginx restart',
-    path    => '/etc/init.d/'
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
