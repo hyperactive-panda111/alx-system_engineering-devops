43276cd0da32eef07a23190ded999bc731e44ebe
Amo Mensah <amomensah300@gmail.com>
2023-05-19 04:03:57 +0000

    adding files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+#increases traffic limit on nginx server
+
+exec  { 'fix-for-nginx':
+    command => 'sed -i "s/15/4096" /etc/default/nginx/',
+    path    => '/usr/local/bin/:bin/'
+}
+
+#restart nginx
+exec { 'nginx-restart':
+    command => 'nginx restart',
+    path    => '/etc/init.d/'
+}
